762a2a4ed4278f15b99f4d4d565f94546ca7cbcf
ethereaxl <115656749+ethereaxl@users.noreply.github.com>
2025-03-14 19:57:40 +0300

    PlayerController
    Тест передвижения и прыжков с помощью капсулы

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public HealthManager healthManager; 
+    public DamageHandler damageHandler; 
+    public MovementController movementController; 
+    public Weapon equippedWeapon; 
+    public int Mana { get; private set; } 
+    public int PhysicalAttack { get; private set; } 
+    public int MagicAttack { get; private set; } 
+
+    private Rigidbody rb; 
+    public float moveSpeed = 5f;
+    private bool isGrounded;
+    public float jumpForce = 5f;
+    public LayerMask groundLayer;
+    private Collider capsuleCollider;
+
+    private void Start()
+    {
+        
+        healthManager = GetComponent<HealthManager>();
+        damageHandler = GetComponent<DamageHandler>();
+        movementController = GetComponent<MovementController>();
+        rb = GetComponent<Rigidbody>();
+
+        
+        rb.useGravity = true;
+
+        Collider capsuleCollider = GetComponent<Collider>();
+        Collider[] allColliders = GetComponentsInChildren<Collider>(); 
+
+        foreach (var otherCollider in allColliders)
+        {
+            if (otherCollider != capsuleCollider)
+            {
+                Physics.IgnoreCollision(capsuleCollider, otherCollider, true);
+            }
+        }
+
+    }
+
+    private void Update()
+    {
+        
+        Move();
+        Jump();
+    }
+
+    
+    private void Move()
+    {
+        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        
+        Vector3 movement = new Vector3(horizontal, 0, vertical).normalized;
+
+        
+        if (movement.magnitude >= 0.1f)
+        {
+            
+            Vector3 moveDirection = transform.forward * movement.z + transform.right * movement.x;
+            rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.deltaTime);
+        }
+    }
+    private void Jump()
+    {
+        if(Mathf.Abs(transform.position.y - 1f) < 0.1f && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+
+    }
+
+    public void ReceiveDamage(int amount, DamageType type, float physicalResistance, float magicResistance)
+    {
+        int finalDamage = damageHandler.CalculateDamage(amount, type, physicalResistance, magicResistance);
+        healthManager.TakeDamage(finalDamage);
+    }
+
+    
+    public void DealDamage(Enemy target, int damage, DamageType type)
+    {
+        equippedWeapon?.Attack(target, damage, type);
+    }
+
+    
+    public void EquipWeapon(Weapon weapon)
+    {
+        equippedWeapon = weapon;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"Collision detected with: {collision.gameObject.name}");
+    }
+
+}
